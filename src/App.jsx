1113035ec12c9419308a3d64bb876aae306842d6
bubble-code <aobregonrodri@gmail.com>
2022-12-15 11:32:22 +0100

    before install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { RouterProvider, Outlet } from 'react-router-dom'
+import { router } from './routes'
+import { NavBar } from './component'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <RouterProvider router={router}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <NavBar />
+        <div className="App">
+          <header className="App-header">
+            <h1>App</h1>
+          </header>
+          <Outlet />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </RouterProvider>
   )
 }
 
