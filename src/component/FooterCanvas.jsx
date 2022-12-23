9e7a2e5b5f1c3d83a394f965eb9076b6704a6765
bubble-code <aobregonrodri@gmail.com>
2022-12-23 15:40:21 +0100

    ;

--- src/component/FooterCanvas.jsx
+++ src/component/FooterCanvas.jsx
@@ -1,45 +1,39 @@
-import { useEffect, useRef } from "react"
+import * as THREE from 'three';
 import { OrbitControls } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Color, Vector3, Quaternion } from "three";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { useMemo, useRef } from "react";
+
+
 
 export const FooterCanvas = () => {
-  const meshRef = useRef();
-  const quaternion = new Quaternion();
-
-  useEffect(() => {
-    console.log("sdsdsds", meshRef.current);
-    const currentPositions = meshRef.current.position;
-    // Copy the attributes
-    const originalPositions = currentPositions.clone();
-    const originalPositionsArray = originalPositions?.array || [];
-
-    // Go through each vector (series of 3 values) and modify the values
-    for (let i = 0; i < originalPositionsArray.length; i = i + 3) {
-      const modifiedPositionVector = new Vector3(originalPositionsArray[i], originalPositionsArray[i + 1], originalPositionsArray[i + 2]);
-      const upVector = new Vector3(0, 1, 0);
-
-      // Rotate along the y axis (0, 1, 0)
-      quaternion.setFromAxisAngle(
-        upVector,
-        (Math.PI / 180) * (modifiedPositionVector.y + 10) * 100 // the higher along the y axis the vertex is, the more we rotate
-      );
-      modifiedPositionVector.applyQuaternion(quaternion);
-
-      // Apply the modified position vector coordinates to the current position attributes array
-      currentPositions.array[i] = modifiedPositionVector.x
-      currentPositions.array[i + 1] = modifiedPositionVector.y
-      currentPositions.array[i + 2] = modifiedPositionVector.z
+  const pointsRef = useRef();
+  const pointTexture = useLoader(THREE.TextureLoader, 'circle.png');
+  const count = 2000;
+  const paticulesPosition = useMemo(() => {
+    const position = new Float32Array(count * 3);
+    for (let i = 0; i < count; i++) {
+      let x = (Math.random() - 0.5) * 2;
+      let y = (Math.random() - 0.5) * 2;
+      let z = (Math.random() - 0.5) * 2;
+      position.set([x, y, z], i * 3);
     }
-    // Set the needsUpdate flag to "true"
-    currentPositions.needsUpdate = true;
-  }, [])
+    return position;
+  }, [count])
 
   return (
-    <mesh ref={meshRef} >
-      <boxGeometry args={[1, 1, 1, 10, 10, 10]} />
-      <meshLambertMaterial color="hotpink" emissive="hotpink" />
-    </mesh>
+    <points ref={pointsRef}>
+      <bufferGeometry attach='geometry' >
+        <bufferAttribute
+          attach="attributes-position"
+          count={count}
+          array={paticulesPosition}
+          itemSize={3}
+          normalized={false}
+        />
+      </bufferGeometry>
+
+      <pointsMaterial color="#5786f5" size={0.015} sizeAttenuation transparent map={pointTexture} />
+    </points>
   )
 
-}
+};
